a32732a558d99a1d2431b672d8cd3774a12c5ac1
D-Chenkov <ChenkovvoknehC@gmail.com>
2022-03-08 20:15:36 -0800

    RoleAssignment Linted

--- server/poc.js
+++ server/poc.js
@@ -20,6 +20,58 @@ class GameEventsManager {
     this.listeners[event].map((item) => item.update(data));
   }
 }
+class Player {
+  constructor(name, x, y) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+  }
+
+  proximity(hider) {
+    const first = (hider.x - this.x) ** 2;
+    const second = (hider.y - this.y) ** 2;
+    return Math.sqrt(first + second);
+  }
+
+  moveTo(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  goCrazy() {
+    this.interval = setInterval(() => {
+      this.moveTo(randInt(100), randInt(100));
+    }, 1 * 1000);
+    setTimeout(() => {
+      clearInterval(this.interval);
+    }, 60 * 1000);
+  }
+}
+
+class Hider extends Player {
+  constructor(name, x, y) {
+    super(name, x, y);
+    this.alive = true;
+  }
+
+  update(seeker) {
+    const distance = this.proximity(seeker);
+    if (distance < 3) {
+      this.alive = false;
+      console.log(`${this.name} is dead`);
+    }
+    console.log(`${this.name}: Seeker is ${distance.toFixed(2)}m away`);
+  }
+}
+
+class Seeker extends Player {
+  update(hiders) {
+    const encode = (index) => String.fromCharCode(index + 65);
+    hiders
+      .map((hider) => this.proximity(hider).toFixed(2))
+      .map((distance, index) => console.log(`${this.name}: Hider${encode(index)} is ${distance}m away`));
+  }
+}
 
 class Game {
   constructor(id) {
@@ -31,7 +83,7 @@ class Game {
   }
 
   addHider(player) {
-    const hider = new Hider(player.name, player.x, player.y); 
+    const hider = new Hider(player.name, player.x, player.y);
     hider.goCrazy();
     this.hiders.push(hider);
     this.eventsManager.subscribe('hide', hider);
@@ -83,85 +135,26 @@ class Game {
     clearInterval(this.checkInterval);
   }
 
-  //Assuming we get list of players from lobby
-  addPlayer(fren){
-    this.players.push(fren)
+  addPlayer(fren) {
+    this.players.push(fren);
   }
 
   assignRoles() {
-    const hiderId = randInt(this.players.length)
-    this.addSeeker(this.players[hiderId])
-    this.players.splice(hiderId, 1)
-    this.players.forEach(fren => this.addHider(fren));
-  }
-}
-
-class Player {
-  constructor(name, x, y) {
-    this.name = name;
-    this.x = x;
-    this.y = y;
-  }
-
-  proximity(hider) {
-    const first = (hider.x - this.x) ** 2;
-    const second = (hider.y - this.y) ** 2;
-    return Math.sqrt(first + second);
-  }
-
-  moveTo(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  goCrazy() {
-    this.interval = setInterval(() => {
-      this.moveTo(randInt(100), randInt(100));
-    }, 1 * 1000);
-    setTimeout(() => {
-      clearInterval(this.interval);
-    }, 60 * 1000);
-  }
-
-  
-}
-
-class Hider extends Player {
-  constructor(name, x, y) {
-    super(name, x, y);
-    this.alive = true;
-  }
-
-  update(seeker) {
-    const distance = this.proximity(seeker);
-    if (distance < 3) {
-      this.alive = false;
-      console.log(`${this.name} is dead`);
-    }
-    console.log(`${this.name}: Seeker is ${distance.toFixed(2)}m away`);
-  }
-}
-
-class Seeker extends Player {
-  update(hiders) {
-    const encode = (index) => String.fromCharCode(index + 65);
-    hiders
-      .map((hider) => this.proximity(hider).toFixed(2))
-      .map((distance, index) => console.log(`${this.name}: Hider${encode(index)} is ${distance}m away`));
+    const hiderId = randInt(this.players.length);
+    this.addSeeker(this.players[hiderId]);
+    this.players.splice(hiderId, 1);
+    this.players.forEach((fren) => this.addHider(fren));
   }
 }
 
 const partida = new Game('XJSDG');
 
-//Instancing actually done in-lobby
-const daw = new Player('Daw', 5, 2) 
-const mizael = new Player('Mizael', 6, 6) 
-const dagz = new Player('DAGZ', 1, 5)
+const daw = new Player('Daw', 5, 2);
+const mizael = new Player('Mizael', 6, 6);
+const dagz = new Player('DAGZ', 1, 5);
 
 partida.addPlayer(daw);
 partida.addPlayer(mizael);
 partida.addPlayer(dagz);
-partida.assignRoles(); 
-
-//console.log(partida);
+partida.assignRoles();
 partida.start();
