6aa03402fb5f7accfa9a701b0f35d2984cb7014a
Christian Cuevas <ccpotential@gmail.com>
2022-03-24 01:56:27 -0700

    Refine Lobby session

--- client/pages/lobby/[session].js
+++ client/pages/lobby/[session].js
@@ -1,49 +1,57 @@
 import { useState } from "react";
-import { useRouter } from "next/router";
 import {
-    Page,
-    Navbar,
-    NavLeft,
-    NavTitle,
-    NavRight,
-    List,
-    ListItem,
-    Button,
+	Page,
+	Navbar,
+	NavTitle,
+	NavRight,
+	List,
+	ListItem,
+	Button,
 } from "framework7-react";
 
 export default function Lobby(props) {
-    console.log(props);
-    const { session, f7router } = props;
-    const [isHost, setIsHost] = useState(true);
+	const { f7router, session } = props;
 
-    const [username, setUsername] = useState("");
-    // const [session, setSession] = useState("");
+	const [isHost, setIsHost] = useState(true);
+	const [self, setSelf] = useState("");
+	const [users, setUsers] = useState(Array.from(Array(100).keys()));
 
-    const [users, setUsers] = useState(Array.from(Array(100).keys()));
+	const closeSession = (e) => {
+		e.preventDefault();
+		console.log(`Closing DotHidden session #${session}...`);
+		f7router.navigate("/");
+	};
 
-    return (
-        <Page noSwipeback>
-            <Navbar>
-                <NavTitle className="text-align-center">
+	const kickUser = (e, user) => {
+		e.preventDefault();
+		if (!isHost)
+			return;
+		console.log(`Kicking user '${user}' from session #${session}...`);
+	};
+
+	return (
+		<Page noSwipeback>
+			<Navbar>
+				<NavTitle className="text-align-center">
                     DotHidden #{session}
-                </NavTitle>
+				</NavTitle>
 
-                <NavRight>
-                    <Button color="red">{isHost ? "CLOSE" : "LEAVE"}</Button>
-                </NavRight>
-            </Navbar>
+				<NavRight>
+					<Button color="red" onClick={closeSession}>{isHost ? "CLOSE" : "LEAVE"}</Button>
+				</NavRight>
+			</Navbar>
 
-            <List simpleList className="no-margin">
-                {users.map((user, index) => (
-                    <ListItem key={index} title={user + 1}>
-                        <div className="item-after">
-                            {isHost &&
-                                <Button outline color="red">Kick</Button>
-                            }
-                        </div>
-                    </ListItem>
-                ))}
-            </List>
-        </Page >
-    );
+			<List simpleList className="no-margin">
+				{users.map((user, index) => (
+					<ListItem key={index} title={user + 1}>
+						<div className="item-after">
+							{isHost &&
+                                <Button outline color="red" onClick={(e) => kickUser(e, user)}>Kick</Button>
+							}
+						</div>
+					</ListItem>
+				))}
+			</List>
+		</Page >
+	);
 }
