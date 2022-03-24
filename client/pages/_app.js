11e20b2ed80b549964a424897f262d67be5a3777
Christian Cuevas <ccpotential@gmail.com>
2022-03-24 01:33:00 -0700

    Lint for tabs

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,34 +7,34 @@ import "framework7/framework7-bundle.css";
 Framework7.use(Framework7React);
 
 const routes = [
-  {
-    path: "/",
-    asyncComponent: () => import("./index"),
-  },
-  {
-    path: "/lobby/:session",
-    asyncComponent: () => import("./lobby/[session]"),
-  },
+	{
+		path: "/",
+		asyncComponent: () => import("./index"),
+	},
+	{
+		path: "/lobby/:session",
+		asyncComponent: () => import("./lobby/[session]"),
+	},
 ];
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const url = `http://localhost:3000${router.asPath}`;
+	const router = useRouter();
+	const url = `http://localhost:3000${router.asPath}`;
 
-  return (
-    <App url={url} routes={routes}>
-      <View
-        main
-        browserHistory
-        browserHistorySeparator=""
-        browserHistoryInitialMatch={true}
-        browserHistoryStoreHistory={false}
-        url="/"
-      >
-        <Component initialPage {...pageProps} />
-      </View>
-    </App>
-  );
+	return (
+		<App url={url} routes={routes}>
+			<View
+				main
+				browserHistory
+				browserHistorySeparator=""
+				browserHistoryInitialMatch={true}
+				browserHistoryStoreHistory={false}
+				url="/"
+			>
+				<Component initialPage {...pageProps} />
+			</View>
+		</App>
+	);
 }
 
 export default MyApp;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,61 +1,61 @@
 import { useState } from "react";
 import {
-  Page,
-  Row,
-  LoginScreenTitle,
-  List,
-  ListInput,
-  Button
+	Page,
+	Row,
+	LoginScreenTitle,
+	List,
+	ListInput,
+	Button
 } from "framework7-react";
 
 export default function MainMenu(props) {
-  const { f7router } = props;
+	const { f7router } = props;
 
-  const [session, setSession] = useState("");
+	const [session, setSession] = useState("");
 
-  const joinSession = (e) => {
-    e.preventDefault();
-    if (session.length < 6)
-      return;
-    console.log(`Attempting to join DotHidden session #${session}...`);
-    f7router.navigate(`lobby/${session}`);
-  };
+	const joinSession = (e) => {
+		e.preventDefault();
+		if (session.length < 6)
+			return;
+		console.log(`Attempting to join DotHidden session #${session}...`);
+		f7router.navigate(`lobby/${session}`);
+	};
 
-  const createSession = (e) => {
-    e.preventDefault();
-    console.log("Creating DotHidden session...");
-    // TODO
-    // f7router.navigate("lobby/");
-  };
+	const createSession = (e) => {
+		e.preventDefault();
+		console.log("Creating DotHidden session...");
+		// TODO
+		// f7router.navigate("lobby/");
+	};
 
-  return (
-    <Page noToolbar noNavbar noSwipeback loginScreen className="padding">
-      <List className="padding">
-        <img src="/vercel.svg" alt="" className="page-content display-flex flex-direction-column justify-content-center" />
-        <LoginScreenTitle>
+	return (
+		<Page noToolbar noNavbar noSwipeback loginScreen className="padding">
+			<List className="padding">
+				<img src="/vercel.svg" alt="" className="page-content display-flex flex-direction-column justify-content-center" />
+				<LoginScreenTitle>
           DotHidden
-        </LoginScreenTitle>
-      </List>
+				</LoginScreenTitle>
+			</List>
 
-      <List form onSubmit={joinSession} className="padding-horizontal">
-        <ListInput outline
-          value={session}
-          onInput={(e) => {
-            setSession(e.target.value);
-          }}
-          input={false}
-        >
-          <input slot="input" type="text" placeholder="Game PIN" className="text-align-center" value={session} onChange={e => setSession(e.target.value)} />
-        </ListInput>
+			<List form onSubmit={joinSession} className="padding-horizontal">
+				<ListInput outline
+					value={session}
+					onInput={(e) => {
+						setSession(e.target.value);
+					}}
+					input={false}
+				>
+					<input slot="input" type="text" placeholder="Game PIN" className="text-align-center" value={session} onChange={e => setSession(e.target.value)} />
+				</ListInput>
 
-        <Button className="margin-horizontal" disabled={session.length < 6} onClick={joinSession}>JOIN</Button>
+				<Button className="margin-horizontal" disabled={session.length < 6} onClick={joinSession}>JOIN</Button>
 
-        <Row>
-          <hr className="col-15" />
-        </Row>
+				<Row>
+					<hr className="col-15" />
+				</Row>
 
-        <Button className="margin-horizontal" onClick={createSession}>CREATE</Button>
-      </List>
-    </Page>
-  );
+				<Button className="margin-horizontal" onClick={createSession}>CREATE</Button>
+			</List>
+		</Page>
+	);
 }
