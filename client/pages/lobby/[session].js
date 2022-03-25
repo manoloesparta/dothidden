630bc1d187428b2dc46b0587fdbe319718be19ff
Christian Cuevas <ccpotential@gmail.com>
2022-03-24 21:33:40 -0700

    List users in Lobby

--- client/pages/lobby/[session].js
+++ client/pages/lobby/[session].js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
 	Page,
 	Navbar,
@@ -10,14 +10,12 @@ import {
 } from "framework7-react";
 
 export default function Lobby(props) {
-	const { f7router, session } = props;
+	const { f7router, session, isHost, username } = props;
 
 	if (session === undefined) {
 		f7router.navigate("/");
 	}
 
-	const [isHost, setIsHost] = useState(true);
-	const [self, setSelf] = useState("");
 	const [users, setUsers] = useState(Array.from(Array(100).keys()));
 
 	const closeSession = (e) => {
@@ -33,6 +31,19 @@ export default function Lobby(props) {
 		console.log(`Kicking user '${user}' from session #${session}...`);
 	};
 
+	useEffect(() => {
+		async function getUsers() {
+			let response = await fetch(`http://localhost:8080/game/${session}/players`);
+			
+			if (response.status === 200) {
+				let json_response = await response.json();
+				console.log(json_response);
+				setUsers(json_response || []);
+			}
+		};
+		getUsers();
+	  }, []);
+
 	return (
 		<Page noSwipeback>
 			<Navbar>
@@ -47,7 +58,7 @@ export default function Lobby(props) {
 
 			<List simpleList className="no-margin">
 				{users.map((user, index) => (
-					<ListItem key={index} title={user + 1}>
+					<ListItem key={index} title={user.name}>
 						<div className="item-after">
 							{isHost &&
                                 <Button outline color="red" onClick={(e) => kickUser(e, user)}>Kick</Button>
