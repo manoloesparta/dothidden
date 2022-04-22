79dc7e47859bac345a0b18b5248b20d0679d2d8b
Christian Cuevas <ccpotential@gmail.com>
2022-04-22 16:17:46 -0700

    Validate username

--- client/src/components/UsernameModal.vue
+++ client/src/components/UsernameModal.vue
@@ -2,7 +2,9 @@
   <div id="UsernameModal" class="modal text-center" tabindex="-1">
     <div class="modal-dialog modal-fullscreen">
       <div class="modal-content">
-        <div class="modal-body d-flex flex-column justify-content-center">
+        <div
+          class="modal-body m-5 mx-sm-auto d-flex flex-column justify-content-center"
+        >
           <img
             class="mx-auto mb-4"
             src="../assets/dothidden.svg"
@@ -11,19 +13,28 @@
           />
           <h1 class="h3 mb-3 fw-normal">DotHidden</h1>
 
-          <div class="form-floating">
+          <div class="mb-2">
             <input
               type="text"
-              class="mb-2 form-control text-center"
-              id="usernameInput"
-              placeholder="Username"
               v-model="username"
+              class="form-control text-center"
+              :class="{ 'is-invalid': error.length > 0 }"
+              id="usernameInput"
+              aria-describedby="usernameInputFeedback"
+              placeholder="Username"
             />
-            <label for="usernameInput">Username</label>
+            <div
+              v-if="error.length > 0"
+              id="usernameInputFeedback"
+              class="invalid-feedback"
+            >
+              {{ error }}
+            </div>
           </div>
 
           <button
-            @click="$emit('entered', username)"
+            :disabled="!isValidUsername"
+            @click="enterUsername"
             class="w-100 btn btn-primary"
           >
             {{ isHost ? "Create Lobby" : "Join Lobby" }}
@@ -46,8 +57,25 @@ export default {
   },
   data() {
     return {
+      error: "",
+
       username: "",
     };
+  },
+  computed: {
+    isValidUsername() {
+      return this.username.length >= 3 && this.username.length <= 12;
+    },
+  },
+  methods: {
+    enterUsername() {
+      if (!this.isValidUsername) {
+        this.error = "Username needs to between 3 and 12 characters!";
+        return;
+      }
+
+      this.$emit("entered", this.username);
+    },
   },
 };
 </script>
