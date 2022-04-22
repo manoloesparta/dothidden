07d0b814c7e49fa77b8587b59503024c59403607
Christian Cuevas <ccpotential@gmail.com>
2022-04-21 23:27:51 -0700

    Add functionality

--- client/src/views/MainMenu.vue
+++ client/src/views/MainMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="main-menu d-flex flex-column justify-content-center">
-    <form class="m-5 text-center">
+    <form v-on:submit.prevent class="m-5 text-center">
       <img class="mb-4" src="../assets/dothidden.svg" alt="" width="198" />
       <h1 class="h3 mb-3 fw-normal">DotHidden</h1>
 
@@ -10,19 +10,22 @@
           class="mb-2 form-control text-center"
           id="lobbyIdInput"
           placeholder="Lobby ID"
+          v-model="lobby_id"
         />
-        <label for="lobbyIdInput" center="text-center">Lobby ID</label>
+        <label for="lobbyIdInput">Lobby ID</label>
       </div>
 
-      <button class="w-100 btn btn-primary" type="submit">JOIN</button>
+      <button @click="joinLobby" class="w-100 btn btn-primary">Join</button>
 
       <div class="checkbox mb-3">
         <hr />
       </div>
 
-      <button class="w-100 btn btn-primary" type="submit">CREATE</button>
+      <button @click="createLobby" class="w-100 btn btn-primary">
+        Create Lobby
+      </button>
 
-      <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+      <p class="mt-5 mb-3 text-muted">© 2021</p>
     </form>
   </main>
 </template>
@@ -30,5 +33,18 @@
 <script>
 export default {
   name: "MainMenu",
+  data() {
+    return {
+      lobby_id: "",
+    };
+  },
+  methods: {
+    joinLobby() {
+      console.log(`Joining lobby #${this.lobby_id}...`);
+    },
+    createLobby() {
+      console.log(`Creating lobby...`);
+    },
+  },
 };
 </script>
