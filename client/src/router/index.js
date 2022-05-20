daa8c5523fe7430108fbcf334da36be049e7b39b
Christian Cuevas <ccpotential@gmail.com>
2022-05-19 21:34:49 -0700

    Create GameView

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -15,6 +15,15 @@ const routes = [
     }),
     component: () => import("../views/LobbyView.vue"),
   },
+  {
+    path: "/lobby/:lobby_id/game",
+    name: "lobby",
+    props: (route) => ({
+      is_host: route.params.is_host,
+      lobby_id: route.params.lobby_id,
+    }),
+    component: () => import("../views/GameView.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ client/src/views/GameView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="d-flex flex-column justify-content-center text-center"
+    :class="{
+      'hidding-color': state === 'hidding',
+      'seek-color': state === 'seek',
+      'hide-color': state === 'hide',
+      'caught-color': state === 'caught',
+    }"
+  >
+    <div v-if="state === 'hidding'">
+      <div>
+        <h1 class="display-1"><em>128</em></h1>
+        <br />
+        <h1 class="display-1"><strong>HIDE</strong></h1>
+      </div>
+    </div>
+    <div v-if="state === 'seek'"></div>
+    <div v-if="state === 'hide'"></div>
+    <div v-if="state === 'caught'"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameView",
+  props: {
+    is_host: {
+      type: Boolean,
+      default: false,
+    },
+    lobby_id: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      // Game States:
+      // hidding
+      // seek
+      // hide
+      // caught
+      state: "hidding",
+
+      user: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.hidding-color {
+  background-color: #9932cc;
+}
+
+.seek-color {
+  color: #dc143c;
+}
+
+.hide-color {
+  color: #00bfff;
+}
+
+.caught-color {
+  color: #ff0000;
+}
+</style>
