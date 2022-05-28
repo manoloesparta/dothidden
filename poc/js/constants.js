8f0533e471f16ba8b0cf531ee4775e72fa0d34aa
Manuel Esparza <manoloesparta@gmail.com>
2022-05-28 10:09:30 -0700

    prod poc

--- poc/js/constants.js
+++ poc/js/constants.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8080'
-const WS_BASE_URL = 'ws://localhost:8080'
+const BASE_URL = 'https://api.hidenseek.manoloesparta.com/health'
+const WS_BASE_URL = 'wss://api.hidenseek.manoloesparta.com/health'
 
 // Socket connection
 const socket = io(WS_BASE_URL)
